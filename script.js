c7221b115aea9a270a56d4e34e1fbb5de7db6bd5
nafirdausa <bless.azz35@gmail.com>
2021-11-17 09:13:38 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onscroll = function () {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      $('#content').animate({ top: '0' }, 500);
+      $this = $('#scroll-me');
+      $this.css('text-align', 'left');
+      $this.css('margin', '0px 0px');
+      $this.css('padding', '15px 15px');
+      document.getElementById('scroll-me').innerHTML = '<a href="/">ZOEL</a>';
+      document.getElementById('scroll-me').style.color = 'black';
+
+    }
+
+};
