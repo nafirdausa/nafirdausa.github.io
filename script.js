0268899e5c884b4c18a7637679373f34f12b42ba
nafirdausa <bless.azz35@gmail.com>
2021-11-20 07:08:59 +0700

    memodifikasi tampilan hp merubah margin dan background navbar

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.onscroll = function () {
       $this.css('text-align', 'left');
       $this.css('margin', '0px 0px');
       $this.css('padding', '15px 15px');
+      $this.css('background-color', 'white');
       document.getElementById('scroll-me').innerHTML = '<a href="/">ZOEL</a>';
       document.getElementById('scroll-me').style.color = 'black';
 
